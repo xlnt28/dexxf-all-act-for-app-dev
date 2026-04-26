622f8474cdae0ef3205f268a3c295c6f982c51d0
Dexter Facelo <Dexter101306@gmail.com>
2026-04-26 17:19:43 +0800

    feat: rename

---
+++ flytabnav/ex3/start/Astronomy/Astronomy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Astronomy;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
